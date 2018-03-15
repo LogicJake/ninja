1d7a999ebaea2a1f7e031b54fbdaca63aa6342b5
NepoleonAsus <835410808@qq.com>
2018-03-15 19:56:24 +0800

    键入8开始测试，输出移动时间和距离（pow）

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import pygame
+import time
 from pygame.locals import *
 from sys import exit
 
@@ -23,13 +24,25 @@ offsety = 400
 zhijings = [20,30,50]
 nums = [1,10,20]
 
+stime = 0
+flag = False
+sindex = []
+
 def check(screen,index,x,y):        #检查是否接近圆点
+    global flag,sindex
     x += mouse_cursor.get_width() / 2
     y += mouse_cursor.get_height() / 2
+    i = 0
     for d in index:
         if pow(x-d['x'], 2)+pow(y-d['y'],2) <= pow(zhijing/2,2):
+            if flag and i == 0:
+                eindex = pygame.mouse.get_pos()
+                print("移动距离为："+str(pow(sindex[0] - eindex[0], 2) + pow(sindex[1] - eindex[1], 2)))
+                sindex = []
+                print("时间为："+str(time.time()-stime))
+                flag = False
             pygame.draw.circle(screen, green, [d['x'], d['y']], zhijing)
-
+        i += 1
 if __name__ == '__main__':
     zhijing_index = random.randrange(3)
     num_index = random.randrange(3)
@@ -81,10 +94,20 @@ if __name__ == '__main__':
                 elif event.type == K_ESCAPE:
                     # 接收到退出事件后退出程序
                     exit()
+                elif event.key == K_8:
+                    stime = time.time()
+                    sindex = pygame.mouse.get_pos()
+                    flag = True
+
         screen.fill([255, 255, 255])  # 用白色填充窗口
 
+        i = 0
         for d in index:
-            pygame.draw.circle(screen, red, [d['x'], d['y']],zhijing)
+            if i == 0:
+                pygame.draw.circle(screen, black, [d['x'], d['y']], zhijing)
+            else:
+                pygame.draw.circle(screen, red, [d['x'], d['y']],zhijing)
+            i += 1
 
         pygame.mouse.set_visible(False)
 
